b7c4fd14c1fb7e7c8b673b006f9da53aa4b68a67
Justin S. Baiko <baiko@utexas.edu>
2016-11-11 17:56:07 -0600

    burger controller finished

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,5 +3,27 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 router.get('/', function (req,res) {
-  res.redirect('')
+  res.redirect('/burgers');
 });
+
+router.get('/burgers',function(req,res) {
+  burger.all(function (data) {
+    var hbsObject = {burgers : data};
+  });
+  res.render('index',hbsObject);
+});
+
+router.post('/burgers/create',function (req,res) {
+   burger.insert(req.body.name, function() {
+     res.redirect('/burgers');
+   });
+});
+
+router.put('/burgers/update/:id', function (req,res) {
+  var condition = 'id = ' + req.params.id;
+  burger.update(req.body.name, function () {
+    res.redirect('/burgers');
+  });
+});
+
+module.exports = router;
