c1c59d0dc3477df7c918a201cd0f3e09e2db83cf
Justin S. Baiko <baiko@utexas.edu>
2016-11-11 15:12:23 -0600

    server setup complete

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express');
-const method-overide = require('method-overide');
-const body-parser = require('body-parser');
+const methodOveride = require('method-overide');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+
+var app = express();
+app.use(express.static(process.cwd()+'/public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOveride('_method'));
+app.engine('handlebars',exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controllers.js');
+
+app.use('/', routes);
+
+var port = 3000;
+app.listen(port);
